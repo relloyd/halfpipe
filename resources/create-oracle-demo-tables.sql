c22ef1a2682bf2a7df7bae025d4c698f5619292e
Richard Lloyd <relloyd@gmail.com>
2020-07-08 11:43:42 +0100

    Include demo tables 1-3 only

--- resources/create-oracle-demo-tables.sql
+++ resources/create-oracle-demo-tables.sql
@@ -26,10 +26,9 @@ declare
     end;
 begin
     pr_drop('drop view v_all_objects');
-    pr_drop('drop table hp_demo_delta purge');
-    pr_drop('drop table hp_demo_snapshot purge');
-    pr_drop('drop table hp_demo_sync purge');
-    pr_drop('drop table hp_demo_events purge');
+    pr_drop('drop table hp_demo_1_snapshot purge');
+    pr_drop('drop table hp_demo_2_delta purge');
+    pr_drop('drop table hp_demo_3_sync purge');
     pr_drop('drop sequence seq_hp_demo');
 end;    
 /
@@ -54,7 +53,7 @@ create or replace view v_all_objects as (
 
 create sequence seq_hp_demo start with 1 increment by 1 cache 50;
 
-create table hp_demo_delta (	
+create table hp_demo_2_delta (	
     sequence number,
     owner varchar2(128 byte), 
 	object_name varchar2(128 byte), 
@@ -87,8 +86,8 @@ create table hp_demo_delta (
 
 -- ensure sequence is fixed in the dmeo table...
 
-create or replace trigger trg_hp_demo_delta_seq
-    before insert on hp_demo_delta for each row
+create or replace trigger trg_hp_demo_2_delta_seq
+    before insert on hp_demo_2_delta for each row
     declare
     begin
         :new.sequence := seq_hp_demo.nextval;   
@@ -97,17 +96,17 @@ create or replace trigger trg_hp_demo_delta_seq
 
 -- insert rows 1m...
 
-insert into hp_demo_delta select * from v_all_objects;
+insert into hp_demo_2_delta select * from v_all_objects;
 
 -- create primary key index...
 
-alter table hp_demo_delta add primary key (sequence);
+alter table hp_demo_2_delta add primary key (sequence);
 
--- track changes to the data in table HP_DEMO_DELTA with a trigger that 
+-- track changes to the data in table hp_demo_2_delta with a trigger that 
 -- sets LAST_MODIFIED_DATE on INSERT or UPDATE, for each row.
 
-create or replace trigger trg_hp_demo_delta_lstmod
-    before update or insert on  hp_demo_delta for each row
+create or replace trigger trg_hp_demo_2_delta_lstmod
+    before update or insert on  hp_demo_2_delta for each row
     declare
     begin
       :new.last_modified_date := sysdate;
@@ -116,43 +115,26 @@ create or replace trigger trg_hp_demo_delta_lstmod
 
 -- add an index on LAST_MODIFIED_DATE so we can quickly find changed records...
 
-create index idx_hp_demo_delta_1 on hp_demo_delta (last_modified_date);
+create index idx_hp_demo_2_delta_1 on hp_demo_2_delta (last_modified_date);
 
 -----------------------------------------------------------------------------------------------------------------------
 -- DEMO TABLE FOR 'hp cp snap' COMMAND:
 -----------------------------------------------------------------------------------------------------------------------
 
-create table hp_demo_snapshot as select * from hp_demo_delta;
+create table hp_demo_1_snapshot as select * from hp_demo_2_delta;
 
 -----------------------------------------------------------------------------------------------------------------------
 -- DEMO TABLE FOR 'hp sync batch' COMMAND:
 -----------------------------------------------------------------------------------------------------------------------
 
-create table hp_demo_sync as select * from hp_demo_delta;
-alter table hp_demo_sync add primary key (sequence);
-create index idx_hp_demo_sync_1 on hp_demo_sync (last_modified_date);
+create table hp_demo_3_sync as select * from hp_demo_2_delta;
+alter table hp_demo_3_sync add primary key (sequence);
+create index idx_hp_demo_3_sync_1 on hp_demo_3_sync (last_modified_date);
 
--- ensure sequence is fixed in table HP_DEMO_SYNC...
+-- ensure sequence is fixed in table hp_demo_3_sync...
 
-create or replace trigger trg_hp_demo_sync_seq
-    before insert on hp_demo_sync for each row
-    declare
-    begin
-        :new.sequence := seq_hp_demo.nextval;   
-    end;
-/
-
------------------------------------------------------------------------------------------------------------------------
--- DEMO TABLE FOR 'hp sync events' COMMAND:
------------------------------------------------------------------------------------------------------------------------
-
-create table hp_demo_events as select * from hp_demo_delta;
-alter table hp_demo_events add primary key (sequence);
-
--- ensure sequence is fixed in table HP_DEMO_SYNC...
-
-create or replace trigger trg_hp_demo_events_seq
-    before insert on hp_demo_events for each row
+create or replace trigger trg_hp_demo_3_sync_seq
+    before insert on hp_demo_3_sync for each row
     declare
     begin
         :new.sequence := seq_hp_demo.nextval;   
@@ -164,52 +146,3 @@ create or replace trigger trg_hp_demo_events_seq
 -----------------------------------------------------------------------------------------------------------------------
 
 exit
-
-
------------------------------------------------------------------------------------------------------------------------
--- GENERATE CHANGES...
------------------------------------------------------------------------------------------------------------------------
-
------------------------------------------------------------------------------------------------------------------------
--- HP_DEMO_DELTA:
------------------------------------------------------------------------------------------------------------------------
-
-select count(*) from hp_demo_delta;
-
--- Generate changes to HP_DEMO_DELTA: INSERTs and UPDATEs...
-
-update hp_demo_delta set last_modified_date = sysdate where rownum <= 100000;
-insert into hp_demo_delta select * from v_all_objects where rownum <= 100000;
-commit;
-
--- show me number of rows per last modified date...
-select last_modified_date, count(*) from hp_demo_delta group by last_modified_date order by 1 desc;
-
--- show me a row...
-select * from hp_demo_delta where rownum < 2;
-
------------------------------------------------------------------------------------------------------------------------
--- HP_DEMO_SNAPSHOT:
------------------------------------------------------------------------------------------------------------------------
-
-select count(*) from hp_demo_snapshot;
-
------------------------------------------------------------------------------------------------------------------------
--- HP_DEMO_SYNC:
------------------------------------------------------------------------------------------------------------------------
-
-select count(*) from hp_demo_sync;
-delete from hp_demo_sync where last_modified_date > trunc(sysdate);
-
------------------------------------------------------------------------------------------------------------------------
--- HP_DEMO_EVENTS: INSERTs, UPDATEs, DELETES...
------------------------------------------------------------------------------------------------------------------------
-
-insert into hp_demo_events select * from v_all_objects where rownum <= 100;
-commit;
-
-update hp_demo_events set last_modified_date = sysdate where rownum <= 100;
-commit;
-
-delete from hp_demo_events where rownum <= 100;
-commit;
